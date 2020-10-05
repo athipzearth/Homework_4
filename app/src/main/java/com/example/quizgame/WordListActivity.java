63c2f66ae4240a59d5f56bbe99062edc73318c8a
Promlert Lovichit <promlert@gmail.com>
2020-10-05 09:17:58 +0700

    เพิ่มหน้า details ของคำศัพท์, เพิ่มหน้าเกม

--- app/src/main/java/com/example/quizgame/WordListActivity.java
+++ app/src/main/java/com/example/quizgame/WordListActivity.java
@@ -5,38 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.quizgame.model.WordItem;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class WordListActivity extends AppCompatActivity {
-
-   @Override
-   protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_word_list);
-
-      // สร้าง Layout manager
-      LinearLayoutManager lm = new LinearLayoutManager(WordListActivity.this);
-
-      // สร้าง Adapter object
-      MyAdapter adapter = new MyAdapter();
-
-      // เข้าถึง  RecyclerView ใน layout
-      RecyclerView rv = findViewById(R.id.word_list_recycler_view);
-      rv.setLayoutManager(lm); // กำหนด layout manager ให้กับ RecyclerView
-      rv.setAdapter(adapter); // กำหนด adapter ให้กับ RecyclerView
-   }
-}
-
-class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-
-   private WordItem[] items = {
+   static public WordItem[] items = {
          new WordItem(R.drawable.cat, "CAT"),
          new WordItem(R.drawable.dog, "DOG"),
          new WordItem(R.drawable.dolphin, "DOLPHIN"),
@@ -48,7 +36,32 @@ class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
          new WordItem(R.drawable.koala, "KOALA")
    };
 
-   MyAdapter() {
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_word_list);
+
+      // สร้าง Layout manager
+      LinearLayoutManager lm = new LinearLayoutManager(WordListActivity.this);
+
+      List<WordItem> wordList = Arrays.asList(items);
+      // สร้าง Adapter object
+      MyAdapter adapter = new MyAdapter(WordListActivity.this, wordList);
+
+      // เข้าถึง  RecyclerView ใน layout
+      RecyclerView rv = findViewById(R.id.word_list_recycler_view);
+      rv.setLayoutManager(lm); // กำหนด layout manager ให้กับ RecyclerView
+      rv.setAdapter(adapter); // กำหนด adapter ให้กับ RecyclerView
+   }
+}
+
+class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+   final Context mContext;
+   final List<WordItem> mWordList;
+
+   MyAdapter(Context context, List<WordItem> wordList) {
+      this.mContext = context;
+      this.mWordList = wordList;
    }
 
    @NonNull
@@ -56,29 +69,56 @@ class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
       View v = LayoutInflater.from(parent.getContext())
             .inflate(R.layout.item_word, parent, false);
-      MyViewHolder vh = new MyViewHolder(v);
+      MyViewHolder vh = new MyViewHolder(mContext, v);
       return vh;
    }
 
    @Override
-   public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-      holder.imageView.setImageResource(items[position].imageResId);
-      holder.wordTextView.setText(items[position].word);
+   public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
+      holder.imageView.setImageResource(mWordList.get(position).imageResId);
+      holder.wordTextView.setText(mWordList.get(position).word);
+      holder.item = mWordList.get(position);
    }
 
    @Override
    public int getItemCount() {
-      return items.length;
+      return mWordList.size();
    }
 
    static class MyViewHolder extends RecyclerView.ViewHolder {
+      View rootView;
       ImageView imageView;
       TextView wordTextView;
+      WordItem item;
 
-      MyViewHolder(@NonNull View itemView) {
+      MyViewHolder(final Context context, @NonNull View itemView) {
          super(itemView);
+         rootView = itemView;
          imageView = itemView.findViewById(R.id.image_view);
          wordTextView = itemView.findViewById(R.id.word_text_view);
+
+         rootView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Toast.makeText(context, item.word, Toast.LENGTH_SHORT).show();
+
+               Intent intent = new Intent(context, WordDetailsActivity.class);
+               /*intent.putExtra("word", item.word);
+               intent.putExtra("image", item.imageResId);*/
+
+               String itemJson = new Gson().toJson(item);
+               intent.putExtra("item", itemJson);
+
+               context.startActivity(intent);
+
+               // method chaining
+               /*new AlertDialog.Builder(context)
+                     .setTitle("My Dialog")
+                     .setMessage(item.word)
+                     .setPositiveButton("OK", null)
+                     .show();*/
+            }
+         });
       }
    }
 }
